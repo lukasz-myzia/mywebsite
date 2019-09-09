4d5b8d788cb0934b26aa055920174b0bf9e4da38
Łukasz Myzia <lukasz.myzia@gmail.com>
2019-09-09 10:14:29 +0200

    add: JS, padding, margin

---
+++ main.js
@@ -0,0 +1,13 @@
+alert('🚀Siemka, zaprszam do okna Console🚀');
+
+console.log('Fajnie że zajrzałeś/zajrzałaś');
+
+const name = 'Łukasz';
+const surname = 'Myzia';
+
+console.log(name);
+console.log(surname);
+
+console.log('Łączenie stringów');
+
+console.log(`Imię moje ${name} a nazwisko ${surname}`);
